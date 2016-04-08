14d081eff84a108064cadc0538b3fc37a9744171
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2016-04-09 00:09:14 +0200

    #2 - implement a rule which walks over the AST to validate invalid variable assign alignments

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 eslint-plugin-varspacing
 ========================
 
+[![Code Climate](https://codeclimate.com/github/Sententiaregum/eslint-plugin-varspacing/badges/gpa.svg)](https://codeclimate.com/github/Sententiaregum/eslint-plugin-varspacing)
+[![Issue Count](https://codeclimate.com/github/Sententiaregum/eslint-plugin-varspacing/badges/issue_count.svg)](https://codeclimate.com/github/Sententiaregum/eslint-plugin-varspacing)
+[![Build status](https://ci.appveyor.com/api/projects/status/elpdl78par306ai5?svg=true)](https://ci.appveyor.com/project/Ma27/eslint-plugin-varspacing)
+[![Build Status](https://travis-ci.org/Sententiaregum/eslint-plugin-varspacing.svg?branch=master)](https://travis-ci.org/Sententiaregum/eslint-plugin-varspacing)
+
 This plugin aims to implement a eslint rule like ``object-spacing``, but for variables.
--- test/lib/rules/var-spacing.rule.js
+++ test/lib/rules/var-spacing.rule.js
@@ -8,6 +8,197 @@
  * file that was distributed with this source code.
  */
 
-'use strict';
+var rule   = require('../../../lib/rules/var-spacing.rule');
+var tester = require('eslint').RuleTester;
 
-
+var ruleTester = new tester();
+ruleTester.run('var-spacing', rule, {
+  valid: [{
+    code: [
+      'var foo    = {};',
+      'var foobar = {};'
+    ].join('\n')
+  }, {
+    code: [
+      'var foo = {},',
+      '    bar = {};'
+    ].join('\n')
+  }, {
+    code: [
+      'var foo = {},',
+      '    bar = [1,2,3],',
+      '    baz = {};'
+    ].join('\n')
+  }, {
+    code: [
+      'let foo   = [1,2,3];',
+      'const bar = {};'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'const foo = [1,2,3];',
+      'let bar   = {};'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'var foo = {};',
+      '',
+      'var foobar = {};'
+    ].join('\n')
+  }, {
+    code: [
+      'var foo = {};',
+      'for (var i = 0; i < 10; i++) {',
+      '}'
+    ].join('\n')
+  }, {
+    code: [
+      'var foo = {};',
+      'foo     = [];'
+    ].join('\n')
+  }, {
+    code: [
+      'var foo    = {},',
+      '    foobar = [];',
+      'var baz    = {};'
+    ].join('\n')
+  }, {
+    code: [
+      'var foo;',
+      'let blah = {};'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'var foo = {};',
+      'let blah;'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'var foo    = function () {',
+      '    console.log(arguments);',
+      '};',
+      'var foobar = {};'
+    ].join('\n')
+  }, {
+    code: [
+      'var foo = {};',
+      'var bar;',
+      'var foobar = [];'
+    ].join('\n')
+  }, {
+    code: [
+      'let foo, bar;',
+      'const foobar = {};'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'let foo, bar = {};',
+      'let items    = [];'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'const foo = {}, bar = {};',
+      'let items = [];'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'let foobar = {}, bar;',
+      'let items  = [];'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'const foo = {}, bar = {};',
+      'let items = [];'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'const foo = bar = {};',
+      'let blah  = [];'
+    ].join('\n'),
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }],
+  invalid: [{
+    code: [
+      'var foo  = {};',
+      'var foobar = {};'
+    ].join('\n'),
+    errors: [{
+      message: 'Invalid indent!'
+    }]
+  }, {
+    code: [
+      'var foo = {},',
+      '    bazbaz = []'
+    ].join('\n'),
+    errors: [{
+      message: 'Invalid indent!'
+    }]
+  }, {
+    code: [
+      'var foo = {};',
+      'foo = [];'
+    ].join('\n'),
+    errors: [{
+      message: 'Invalid indent!'
+    }]
+  }, {
+    code: [
+      'var foo    = {},',
+      '    foobar = [];',
+      'var baz   = {};'
+    ].join('\n'),
+    errors: [{
+      message: 'Invalid indent!'
+    }]
+  }, {
+    code: [
+      'const foo = {};',
+      'let bar  = [];'
+    ].join('\n'),
+    errors: [{
+      message: 'Invalid indent!'
+    }],
+    parserOptions: {
+      ecmaVersion: 6
+    }
+  }, {
+    code: [
+      'var foo = function () {',
+      '    console.log(arguments);',
+      '};',
+      'var foobar = {};'
+    ].join('\n'),
+    errors: [{
+      message: 'Invalid indent!'
+    }]
+  }]
+});
